e5ad1a3301da6a7588815359fb9fc2837b819f4f
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-07-22 08:21:11 +0900

    자료구조 기본

--- Chapter01/data_structure_basic.c
+++ Chapter01/data_structure_basic.c
@@ -40,11 +40,60 @@
 
 //ÀÌÁø Å½»ö ¾Ë°í¸®Áò
 
+//int BSearch(int ar[], int len, int target)
+//{
+//	int first = 0;
+//	int last = len - 1;
+//	int mid;
+//
+//	while (first <= last)
+//	{
+//		mid = (first + last) / 2;
+//
+//		if (target == ar[mid])
+//		{
+//			return mid;
+//		}
+//		else
+//		{
+//			if (target < ar[mid])
+//				last = mid - 1;
+//			else
+//				first = mid + 1;
+//		}
+//	}
+//	return -1;
+//}
+//
+//int main(void)
+//{
+//	int arr[] = { 1, 3, 5, 7, 9 };
+//	int idx;
+//
+//	idx = BSearch(arr, sizeof(arr) / sizeof(int), 7);
+//	if (idx == -1)
+//		printf("Å½»ö ½ÇÆĞ \n");
+//	else
+//		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+//
+//	idx = BSearch(arr, sizeof(arr) / sizeof(int), 4);
+//	if (idx == -1)
+//		printf("Å½»ö ½ÇÆĞ \n");
+//	else
+//		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+//
+//	return 0;
+//}
+
+
+//O(logn) ÀÇ ¿¬»êÈ½¼ö
+
 int BSearch(int ar[], int len, int target)
 {
 	int first = 0;
 	int last = len - 1;
 	int mid;
+	int opCount = 0;
 
 	while (first <= last)
 	{
@@ -61,26 +110,37 @@ int BSearch(int ar[], int len, int target)
 			else
 				first = mid + 1;
 		}
+		opCount += 1;
 	}
+	printf("ºñ±³¿¬»êÈ½¼ö : %d \n", opCount);
 	return -1;
 }
 
 int main(void)
 {
-	int arr[] = { 1, 3, 5, 7, 9 };
+	int arr1[500] = { 0, };
+	int arr2[5000] = { 0, };
+	int arr3[50000] = { 0, };
 	int idx;
 
-	idx = BSearch(arr, sizeof(arr) / sizeof(int), 7);
+	idx = BSearch(arr1, sizeof(arr1) / sizeof(int), 1);
 	if (idx == -1)
-		printf("Å½»ö ½ÇÆĞ \n");
+		printf("Å½»ö ½ÇÆĞ \n\n");
 	else
-		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+		printf("Å¸°Ù ÀúÀå ÀÎ´ì½º : %d \n", idx);
 
-	idx = BSearch(arr, sizeof(arr) / sizeof(int), 4);
+	idx = BSearch(arr2, sizeof(arr2) / sizeof(int), 2);
 	if (idx == -1)
-		printf("Å½»ö ½ÇÆĞ \n");
+		printf("Å½»ö ½ÇÆĞ \n\n");
 	else
-		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+		printf("Å¸°Ù ÀúÀå ÀÎ´ì½º : %d \n", idx);
+
+	idx = BSearch(arr3, sizeof(arr3) / sizeof(int), 3);
+	if (idx == -1)
+		printf("Å½»ö ½ÇÆĞ \n\n");
+	else
+		printf("Å¸°Ù ÀúÀå ÀÎ´ì½º : %d \n", idx);
+
 
 	return 0;
 }
