deef80395b7eb7cd9bc8a8fc35a32228c03b832a
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-11-11 19:05:36 +0900

    배열기반 스택

---
+++ Chapter06/ArrayBaseStackMain.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning (disable : 4996)
+#pragma warning (disable : 6001)
+#pragma warning (disable : 6031)
+#pragma warning (disable : 6011)
+
+
+#include "ArrayBaseStack.h"
+
+int main(void)
+{
+	Stack stack;
+	StackInit(&stack);
+
+	SPush(&stack, 1);	SPush(&stack, 2);
+	SPush(&stack, 3);	SPush(&stack, 4);
+	SPush(&stack, 5);	
+
+	while (!SIsEmpty(&stack))
+		printf("%d ", SPop(&stack));
+
+	return 0;
+}
