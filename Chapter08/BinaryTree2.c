cb574933504adc4d9255ad4345dfd9c974e60294
김동현 <qwert6603@naver.com>
2020-04-16 02:06:42 +0900

    트리 순회

---
+++ Chapter08/BinaryTree2.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree2.h"
+
+BTreeNode* MakeBTreeNode(void)
+{
+	BTreeNode* newNode = (BTreeNode*)malloc(sizeof(BTreeNode));
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
+
+BTData GetData(BTreeNode* bt)
+{
+	return bt->data;
+}
+
+void SetData(BTreeNode* bt, BTData data)
+{
+	bt->data = data;
+}
+
+BTreeNode* GetLeftSubTree(BTreeNode* bt)
+{
+	return bt->left;
+}
+
+BTreeNode* GetRightSubTree(BTreeNode* bt)
+{
+	return bt->right;
+}
+
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub)
+{
+	main->left = sub;
+}
+
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub)
+{
+	main->right = sub;
+}
+
+void PreorderTravers(BTreeNode* bt, VisitFucPtr action)
+{
+	if (bt == NULL)
+		return;
+
+	action(bt->data);
+	PreorderTravers(bt->left, action);
+	PreorderTravers(bt->right, action);
+}
+
+void InorderTravers(BTreeNode* bt, VisitFucPtr action)
+{
+	if (bt == NULL)
+		return;
+
+	InorderTravers(bt->left, action);
+	action(bt->data);
+	InorderTravers(bt->right, action);
+}
+
+void PostorderTravers(BTreeNode* bt, VisitFucPtr action)
+{
+	if (bt == NULL)
+		return;
+
+	PostorderTravers(bt->left, action);
+	PostorderTravers(bt->right, action);
+	action(bt->data);
+}
+
+void DeleteTree(BTreeNode* bt)
+{
+	if (bt == NULL)
+		return;
+	
+	DeleteTree(bt->left);
+	DeleteTree(bt->right);
+
+	free(bt);
+}
--- Chapter08/BinaryTreeMain.c
+++ Chapter08/BinaryTreeMain.c
@@ -1,5 +1,117 @@
 #include <stdio.h>
-#include "BinaryTree.h"
+#pragma warning (disable : 4996)
+#pragma warning (disable : 6001)
+#pragma warning (disable : 6011)
+#pragma warning (disable : 6031)
+
+//#include "BinaryTree.h"
+//
+//int main(void)
+//{
+//	BTreeNode* bt1 = MakeBTreeNode();
+//	BTreeNode* bt2 = MakeBTreeNode();
+//	BTreeNode* bt3 = MakeBTreeNode();
+//	BTreeNode* bt4 = MakeBTreeNode();
+//
+//	SetData(bt1, 1);
+//	SetData(bt2, 2);
+//	SetData(bt3, 3);
+//	SetData(bt4, 4);
+//
+//	MakeLeftSubTree(bt1, bt2);
+//	MakeRightSubTree(bt1, bt3);
+//	MakeLeftSubTree(bt2, bt4);
+//
+//	//bt1 왼쪽 자식 노드 데이터 출력
+//	printf("%d \n", GetData(GetLeftSubTree(bt1)));
+//
+//	//bt1 왼쪽 자식 노드의 왼쪽 자식 노드 데이터 출력
+//	printf("%d \n", GetData(GetLeftSubTree(GetLeftSubTree(bt1))));
+//
+//	return 0;
+//}
+
+
+//#include "BinaryTree.h"
+//
+//void InorderTraverse(BTreeNode* bt)
+//{
+//	if (bt == NULL)
+//		return;
+//
+//	InorderTraverse(bt->left);
+//	printf("%d", bt->data);
+//	InorderTraverse(bt->right);
+//}
+//
+//int main(void)
+//{
+//	BTreeNode* bt1 = MakeBTreeNode();
+//	BTreeNode* bt2 = MakeBTreeNode();
+//	BTreeNode* bt3 = MakeBTreeNode();
+//	BTreeNode* bt4 = MakeBTreeNode();
+//
+//	SetData(bt1, 1);
+//	SetData(bt1, 2);
+//	SetData(bt1, 3);
+//	SetData(bt1, 4);
+//
+//	MakeLeftSubTree(bt1, bt2);
+//	MakeRightSubTree(bt1, bt3);
+//	MakeLeftSubTree(bt2, bt4);
+//
+//	InorderTraverse(bt1);
+//
+//		return 0;
+//}
+
+
+//#include "BinaryTree2.h"
+//
+//void ShowIntData(int data);
+//
+//int main(void)
+//{
+//	BTreeNode* bt1 = MakeBTreeNode();
+//	BTreeNode* bt2 = MakeBTreeNode();
+//	BTreeNode* bt3 = MakeBTreeNode();
+//	BTreeNode* bt4 = MakeBTreeNode();
+//	BTreeNode* bt5 = MakeBTreeNode();
+//	BTreeNode* bt6 = MakeBTreeNode();
+//
+//	SetData(bt1, 1);
+//	SetData(bt2, 2);
+//	SetData(bt3, 3);
+//	SetData(bt4, 4);
+//	SetData(bt5, 5);
+//	SetData(bt6, 6);
+//
+//	MakeLeftSubTree(bt1, bt2);
+//	MakeRightSubTree(bt1, bt3);
+//	MakeLeftSubTree(bt2, bt4);
+//	MakeRightSubTree(bt2, bt5);
+//	MakeRightSubTree(bt3, bt6);
+//
+//	PreorderTravers(bt1, ShowIntData);
+//	puts("");
+//	InorderTravers(bt1, ShowIntData);
+//	puts("");
+//	PostorderTravers(bt1, ShowIntData);
+//	puts("");
+//
+//	return 0;
+//}
+//
+//void ShowIntData(int data)
+//{
+//	printf("%d ", data);
+//}
+
+
+#include "BinaryTree2.h"
+
+void ShowIntData(int data);
+void DeleteTree(BTreeNode* bt);
 
 int main(void)
 {
@@ -7,21 +119,28 @@ int main(void)
 	BTreeNode* bt2 = MakeBTreeNode();
 	BTreeNode* bt3 = MakeBTreeNode();
 	BTreeNode* bt4 = MakeBTreeNode();
+	BTreeNode* bt5 = MakeBTreeNode();
+	BTreeNode* bt6 = MakeBTreeNode();
 
 	SetData(bt1, 1);
 	SetData(bt2, 2);
 	SetData(bt3, 3);
 	SetData(bt4, 4);
+	SetData(bt5, 5);
+	SetData(bt6, 6);
 
 	MakeLeftSubTree(bt1, bt2);
 	MakeRightSubTree(bt1, bt3);
 	MakeLeftSubTree(bt2, bt4);
+	MakeRightSubTree(bt2, bt5);
+	MakeRightSubTree(bt3, bt6);
 
-	//bt1 왼쪽 자식 노드 데이터 출력
-	printf("%d \n", GetData(GetLeftSubTree(bt1)));
-
-	//bt1 왼쪽 자식 노드의 왼쪽 자식 노드 데이터 출력
-	printf("%d \n", GetData(GetLeftSubTree(GetLeftSubTree(bt1))));
+	DeleteTree(bt1);
 
 	return 0;
+}
+
+void ShowIntData(int data)
+{
+	printf("%d ", data);
 }
