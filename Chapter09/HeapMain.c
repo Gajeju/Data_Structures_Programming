0e9f0b2cf8079fc165d03daff654186f4a6abe33
김동현 <qwert6603@naver.com>
2020-04-23 12:08:58 +0900

    우선순위 큐

--- Chapter09/HeapMain.c
+++ Chapter09/HeapMain.c
@@ -32,32 +32,93 @@
 //}
 
  
-#include "UsefulHeap.h"
+//#include "UsefulHeap.h"
+//
+//int DataPriorityComp(char ch1, char ch2)
+//{
+//    return ch2 - ch1;
+//}
+//
+//int main(void)
+//{
+//    Heap heap;
+//    HeapInit(&heap, DataPriorityComp);
+//
+//   	HInsert(&heap, 'A');
+//   	HInsert(&heap, 'B');
+//   	HInsert(&heap, 'C');
+//   	printf("%c \n", HDelete(&heap));
+//    
+//   	HInsert(&heap, 'A');
+//   	HInsert(&heap, 'B');
+//   	HInsert(&heap, 'C');
+//   	printf("%c \n", HDelete(&heap));
+//    
+//   	while (!HIsEmpty(&heap))
+//   	{
+//   		printf("%c \n", HDelete(&heap));
+//   	}
+//
+//    return 0;
+//}
 
-int DataPriorityComp(char ch1, char ch2)
+
+//#include "PriorityQueue.h"
+//
+//int DataPriorityComp(char ch1, char  ch2)
+//{
+//    return ch2 - ch1;
+//}
+//
+//int main(void)
+//{
+//    PQueue pq;
+//    PQueueInit(&pq, DataPriorityComp);
+//
+//    PEnqueue(&pq, 'A');
+//    PEnqueue(&pq, 'B');
+//    PEnqueue(&pq, 'C');
+//    printf("%c \n", PDequeue(&pq));
+//
+//    PEnqueue(&pq, 'A');
+//    PEnqueue(&pq, 'B');
+//    PEnqueue(&pq, 'C');
+//    printf("%c \n", PDequeue(&pq));
+//
+//    while (!PQIsEmpty(&pq))
+//        printf("%c \n", PDequeue(&pq));
+//     
+//
+//    return 0;
+//}
+
+
+
+#include "PriorityQueue.h"
+#include <String.h>
+
+int DataPriorityComp(char* str1, char* str2)
 {
-    return ch2 - ch1;
+    return sizeof(str1) - sizeof(str2);
 }
 
 int main(void)
 {
-    Heap heap;
-    HeapInit(&heap, DataPriorityComp);
+    PQueue pq;
+    PQueueInit(&pq, DataPriorityComp);
 
-   	HInsert(&heap, 'A');
-   	HInsert(&heap, 'B');
-   	HInsert(&heap, 'C');
-   	printf("%c \n", HDelete(&heap));
-    
-   	HInsert(&heap, 'A');
-   	HInsert(&heap, 'B');
-   	HInsert(&heap, 'C');
-   	printf("%c \n", HDelete(&heap));
-    
-   	while (!HIsEmpty(&heap))
-   	{
-   		printf("%c \n", HDelete(&heap));
-   	}
+    PEnqueue(&pq, "Good Morning");
+    PEnqueue(&pq, "AAA");
+    PEnqueue(&pq, "BBBB");
+ 
+
+    PEnqueue(&pq, "Good Morning");
+    PEnqueue(&pq, "AAA");
+    PEnqueue(&pq, "BBBB");
+    printf("%s \n", PDequeue(&pq));
+
+    while (!PQIsEmpty(&pq))
+        printf("%s \n", PDequeue(&pq));
 
     return 0;
 }
