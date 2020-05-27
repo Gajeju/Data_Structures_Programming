c417091f636b7515e61ab36c4d4a69cbcb63a06b
김동현 <qwert6603@naver.com>
2020-05-27 21:23:55 +0900

    이진 탐색 트리

---
+++ Chapter11/BinaryTree2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree2.h"
+
+BTreeNode* MakeBTreeNode(void)
+{
+	BTreeNode* newNode = (BTreeNode*)malloc(sizeof(BTreeNode));
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
+
+BTData GetData(BTreeNode* bt)
+{
+	return bt->data;
+}
+
+void SetData(BTreeNode* bt, BTData data)
+{
+	bt->data = data;
+}
+
+BTreeNode* GetLeftSubTree(BTreeNode* bt)
+{
+	return bt->left;
+}
+
+BTreeNode* GetRightSubTree(BTreeNode* bt)
+{
+	return bt->right;
+}
+
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub)
+{
+	main->left = sub;
+}
+
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub)
+{
+	main->right = sub;
+}
+
--- Chapter11/interpolSearch.c
+++ Chapter11/interpolSearch.c
@@ -1,51 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#pragma warning (disable : 4996)
-#pragma warning (disable : 6011)
-#pragma warning (disable : 6001)
-#pragma warning (disable : 6031)
-
-int ISearch(int ar[], int first, int last, int target)
-{
-	int mid;
-
-	if (ar[first] > target || ar[last] < target)
-		return -1;
-
-	mid = ((double)(target - ar[first]) / (ar[last] - ar[first]) * (last - first)) + first;
-
-	if (ar[mid] == target)
-		return mid;
-	else if (target < ar[mid])
-		return ISearch(ar, first, mid - 1, target);
-	else
-		return ISearch(ar, mid + 1, last, target);
-}
-
-
-
-int main(void)
-{
-	int arr[] = { 1, 3, 5, 7, 9 };
-	int idx;
-	
-	idx = ISearch(arr, 0, sizeof(arr) / sizeof(int) - 1, 7);
-	if (idx == -1)
-		printf("Å½»ö ½ÇÆÐ \n");
-	else
-		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
-
-	idx = ISearch(arr, 0, sizeof(arr) / sizeof(int) - 1, 10);
-	if (idx == -1)
-		printf("Å½»ö ½ÇÆÐ \n");
-	else
-		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
-
-	idx = ISearch(arr, 0, sizeof(arr) / sizeof(int) - 1, 2);
-	if (idx == -1)
-		printf("Å½»ö ½ÇÆÐ \n");
-	else
-		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#pragma warning (disable : 4996)
+//#pragma warning (disable : 6011)
+//#pragma warning (disable : 6001)
+//#pragma warning (disable : 6031)
+//
+//int ISearch(int ar[], int first, int last, int target)
+//{
+//	int mid;
+//
+//	if (ar[first] > target || ar[last] < target)
+//		return -1;
+//
+//	mid = ((double)(target - ar[first]) / (ar[last] - ar[first]) * (last - first)) + first;
+//
+//	if (ar[mid] == target)
+//		return mid;
+//	else if (target < ar[mid])
+//		return ISearch(ar, first, mid - 1, target);
+//	else
+//		return ISearch(ar, mid + 1, last, target);
+//}
+//
+//
+//
+//int main(void)
+//{
+//	int arr[] = { 1, 3, 5, 7, 9 };
+//	int idx;
+//	
+//	idx = ISearch(arr, 0, sizeof(arr) / sizeof(int) - 1, 7);
+//	if (idx == -1)
+//		printf("Å½»ö ½ÇÆÐ \n");
+//	else
+//		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+//
+//	idx = ISearch(arr, 0, sizeof(arr) / sizeof(int) - 1, 10);
+//	if (idx == -1)
+//		printf("Å½»ö ½ÇÆÐ \n");
+//	else
+//		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+//
+//	idx = ISearch(arr, 0, sizeof(arr) / sizeof(int) - 1, 2);
+//	if (idx == -1)
+//		printf("Å½»ö ½ÇÆÐ \n");
+//	else
+//		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º : %d \n", idx);
+//
+//	return 0;
+//}
