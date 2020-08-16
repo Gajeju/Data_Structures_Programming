82519d303efa9cc0def33273d01963b1ef74a8e6
제주 제주 <qwert6603@naver.com>
2020-08-16 14:49:11 +0900

    AVL트리

---
+++ Chapter12/AVLRebalance.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "BinaryTree3.h"
+
+BTreeNode* Rebalance(BTreeNode** pRoot);
---
+++ Chapter12/BinaryTree3.h
@@ -0,0 +1,33 @@
+#pragma once
+
+//BinaryTree2.h 에서 SearchTree의 노드 제거의 구현을 위한 함수 추가
+
+typedef int BTData;
+
+typedef struct _bTreeNode
+{
+	BTData data;
+	struct _bTreeNode* left;
+	struct _bTreeNode* right;
+} BTreeNode;
+
+BTreeNode* MakeBTreeNode(void);
+BTData GetData(BTreeNode* bt);
+void SetData(BTreeNode* bt, BTData data);
+
+BTreeNode* GetLeftSubTree(BTreeNode* bt);
+BTreeNode* GetRightSubTree(BTreeNode* bt);
+
+void MakeLeftSubTree(BTreeNode* main, BTreeNode* sub);
+void MakeRightSubTree(BTreeNode* main, BTreeNode* sub);
+
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode* bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode* bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode* bt, VisitFuncPtr action);
+
+BTreeNode* RemoveLeftSubTree(BTreeNode* bt);
+BTreeNode* RemoveRightSubTree(BTreeNode* bt);
+void ChangeLeftSubTree(BTreeNode* main, BTreeNode* sub);
+void ChangeRightSubTree(BTreeNode* main, BTreeNode* sub);
