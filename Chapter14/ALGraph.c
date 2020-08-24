788e6a9dc116fcd599e3aa1729e33883426b9159
제주 제주 <qwert6603@naver.com>
2020-08-24 19:07:06 +0900

    인접 리스트 기반 그래프 구현

---
+++ Chapter14/ALGraph.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ALGraph.h"
+
+//초기화
+void GraphInit(ALGraph* pg, int nv)
+{
+
+}
+
+//리소스 해제
+void GraphDestroy(ALGraph* pg)
+{
+
+}
+
+//간선 추가
+void AddEdge(ALGraph* pg, int fromV, int toV)
+{
+
+}
+
+//간선 정보 출력
+void ShowGraphEdgeInfo(ALGraph* pg)
+{
+
+}
---
+++ Chapter14/ALGraphMain.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#pragma warning (disable : 4996)
+
+
+#include "ALGraph.h"
+
+int main(void)
+{
+	A
+}
