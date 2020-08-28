95dbf5bbca4b74414725f797ee196e82554b328f
제주 제주 <qwert6603@naver.com>
2020-08-28 22:23:22 +0900

    DFS 기반 그래프 탐색

--- Chapter14/ALGraphDFS.c
+++ Chapter14/ALGraphDFS.c
@@ -1,11 +1,80 @@
 #include <stdio.h>
 #include "ALGraphDFS.h"
+#include "ArrayBaseStack.h"
 
+int WhoIsPrecede(int data1, int data2);
 
-void GraphInit(ALGraph* pg, int numV);
-void GraphDestroy(ALGraph* pg);
-void AddEdge(ALGraph* pg, int formV, int toV);
-void ShowGraphEdgeInfo(ALGraph* pg);
+void GraphInit(ALGraph* pg, int nv)
+{
+	pg->adjList = (List*)malloc(sizeof(List) * nv);
+
+	for (int i = 0; i < nv; i++)
+	{
+		ListInit(&(pg->adjList[i]));
+		SetSortRule(&(pg->adjList[i]), WhoIsPrecede);
+	}
+	pg->visitInfo = (int*)malloc(sizeof(int) * pg->numV);
+	memset(pg->visitInfo, 0, sizeof(int) * pg->numV);
+}
+
+void GraphDestroy(ALGraph* pg)
+{
+	if (pg->adjList != NULL)
+		free(pg->adjList);
+
+	if (pg->visitInfo != NULL)
+		free(pg->visitInfo);
+}
+
+void AddEdge(ALGraph* pg, int fromV, int toV)
+{
+	LInsert(&(pg->adjList[fromV]), toV);
+	LInsert(&(pg->adjList[toV]), fromV);
+
+	pg->numE++;
+}
+
+int WhoIsPrecede(int data1, int data2)
+{
+	if (data1 < data2)
+		return 0;
+	else
+		return 1;
+}
+void ShowGraphEdgeInfo(ALGraph* pg)
+{
+	int vx;
+
+	for (int i = 0; i < pg->numV; i++)
+	{
+		printf("%d와 연결된 정점: ", i + 65);
+
+		if (LFirst(&(pg->adjList[i]), &vx))
+		{
+			printf("%c ", vx);
+			while (LNext(&(pg->adjList[i]), &vx))
+			{
+				printf("%c ", vx);
+			}
+		}
+		puts("");
+	}
+}
+
+//정점의 방문을 진행
+int VisitVertex(ALGraph* pg, int visitV)
+{
+	if (pg->visitInfo[visitV] == 0)
+	{
+		pg->visitInfo[visitV] = 1;
+		printf("%c ", visitV + 65);
+	}
+	return FALSE;
+}
 
 //Deph First Search 기반으로 정점정보 출력
-void DFShowGraphVertex(ALGraph* pg, int startV);
+void DFShowGraphVertex(ALGraph* pg, int startV)
+{
+	Stack stack;
+
+}
--- Chapter14/ALGraphDFS.h
+++ Chapter14/ALGraphDFS.h
@@ -10,9 +10,9 @@ typedef struct _ual
 	int* visitInfo;
 } ALGraph;
 
-void GraphInit(ALGraph* pg, int numV);
+void GraphInit(ALGraph* pg, int nv);
 void GraphDestroy(ALGraph* pg);
-void AddEdge(ALGraph* pg, int formV, int toV);
+void AddEdge(ALGraph* pg, int froV, int toV);
 void ShowGraphEdgeInfo(ALGraph* pg);
 
 //Deph First Search 기반으로 정점정보 출력
